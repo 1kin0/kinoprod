190433e5e42795fb15641c40cfabdfd4ee86f324
kinovw <67692844+kinovw@users.noreply.github.com>
2026-02-14 10:07:53 +0300

    header fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header.jsx'
 import Dither from './components/Dither.jsx'
 
 function App() {
