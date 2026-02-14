0d0a98cc7a11bd324af9e4e4cda7c121f7b645b2
kinovw <67692844+kinovw@users.noreply.github.com>
2026-02-14 21:20:00 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,52 @@ import LightRays from './components/effects/LightRays.jsx'
 import CurvedLoop from './components/effects/CurvedLoop.jsx'
 import ShinyText from './components/effects/ShinyText.jsx'
 import SpotlightCard from './components/effects/SpotlightCard.jsx'
+import CardSwap, { Card } from './components/effects/CardSwap.jsx'
 
 //Vercel
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
+
+function CreateProjectCards() {
+  const projectsList = {
+    first: {
+      description: 'my test first project abla blo bloa lblaadoe with testing test lal lao',
+      title: 'first',
+      link: 'https://github.com'
+    },
+    second: {
+      description: 'my test first project abla blo bloa lblaadoe with testing test lal lao',
+      title: 'second',
+      link: 'https://roblox.com'
+    },
+    third: {
+      description: 'my test first project abla blo bloa lblaadoe with testing test lal lao',
+      title: 'third',
+      link: 'https://microsoft.com'
+    },
+    thirdV2: {
+      description: 'my test first project abla blo bloa lblaadoe with testing test lal lao',
+      title: 'thirdV2',
+      link: 'https://microsoft.com'
+    },
+    quad: {
+      description: 'my test first project abla blo bloa lblaadoe with testing test lal lao',
+      title: 'quad',
+      link: 'https://google.com'
+    }
+  };
+  
+  return (
+    <>
+      {Object.values(projectsList).map((project, index) => (
+        <SpotlightCard key={project.title || index}>
+          <h2 className="text-xl font-bold">{project.title}</h2>
+          <span className="text-gray-400">{project.description}</span>
+        </SpotlightCard>
+      ))}
+    </>
+  );
+}
 
 
 function App() {
@@ -45,7 +87,40 @@ function App() {
       </section>
 
       <section className='bg-gray-800 text-gray-300 w-lvw min-h-svh flex flex-col items-center justify-center relative overflow-hidden' id='about'>
-        <h1 className='p-2 text-4xl'>Introducing</h1>
+        <h1 className='p-2 text-4xl z-1'>Introducing</h1>
+        <img src="/src/assets/arrow.svg" alt="arrow" className='z-1 w-3xs'/>
+        <CardSwap
+            cardDistance={70}
+            verticalDistance={100}
+            delay={4000}
+            pauseOnHover={false}
+          >
+
+            <Card>
+              <h3>Card 1</h3>
+              <p>Your content here</p>
+            </Card>
+
+            <Card>
+              <h3>Card 2</h3>
+              <p>Your content here</p>
+            </Card>
+
+            <Card>
+              <h3>Card 3</h3>
+              <p>Your content here</p>
+            </Card>
+
+            <Card>
+              <h3>Card 4</h3>
+              <p>Your content here</p>
+            </Card>
+
+            <Card>
+              <h3>Card 5</h3>
+              <p>Your content here</p>
+            </Card>
+          </CardSwap>
       </section>
 
       <section className= 'w-lvw min-h-svh flex flex-col items-center justify-center relative overflow-hidden' id='skills'>
@@ -68,27 +143,25 @@ function App() {
           />
         </div>
         <h1 className='p-2 text-4xl'>Skills</h1>
+        <img src="/src/assets/stroke.svg" alt="arrow" className='z-1 w-3xs'/>
       </section>
 
       <section className='bg-gray-800 text-gray-300 w-lvw min-h-svh flex flex-col items-center justify-center relative overflow-hidden py-12' id='projects'>
         <h1>Projects</h1>
         <p className='text-gray-500'>A lot of unusual works</p>
         <div id='card-grid'>
-          <SpotlightCard className="item-card" spotlightColor="#1E2839"></SpotlightCard>
-          <SpotlightCard className="item-card" spotlightColor="#1E2839"></SpotlightCard>
-          <SpotlightCard className="item-card" spotlightColor="#1E2839"></SpotlightCard>
-          <SpotlightCard className="item-card" spotlightColor="#1E2839"></SpotlightCard>
-          <SpotlightCard className="item-card" spotlightColor="#1E2839"></SpotlightCard>
+          {CreateProjectCards()}
         </div>
       </section>
 
       <section className= 'w-lvw min-h-svh flex flex-col items-center justify-center relative overflow-hidden' id='contact'>
         <div className='text-5xl'>
           <ShinyText
-            text='Dm me <3'
+            text='Dm me'
             speed={2}
           />
         </div>
+        <img src="/src/assets/heart.svg" alt="arrow" className='m-6 w-18'/>
       </section>
 
       <Analytics/>
--- src/components/effects/SpotlightCard.jsx
+++ src/components/effects/SpotlightCard.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react';
 
-const SpotlightCard = ({ children, className = '', spotlightColor = 'rgba(255, 255, 255, 0.25)' }) => {
+const SpotlightCard = ({ children, className = '', spotlightColor = '#1E2839', cardId = 'item-card' }) => {
   const divRef = useRef(null);
   const [isFocused, setIsFocused] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -40,6 +40,7 @@ const SpotlightCard = ({ children, className = '', spotlightColor = 'rgba(255, 2
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       className={`relative rounded-3xl border border-neutral-800 bg-neutral-900 overflow-hidden p-8 ${className}`}
+      id = {cardId}
     >
       <div
         className="pointer-events-none absolute inset-0 opacity-0 transition-opacity duration-500 ease-in-out"
